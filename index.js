d10cc2ee0f90c0b3d745508de4505d96eecd41be
Talon Grayson <talongrayson@gmail.com>
2024-08-31 16:06:24 +0100

    Run in a loop

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 var rfid=require('node-rfid');
- 
-rfid.readintime(5000, function(err,result){
+
+for(i=0; i<10; i++) {
     console.log("Listening to RFID for 5 seconds...");
-    if(err) console.error("Sorry, some hardware error occurred: %o", err); //some kind of hardware/wire error
-    
-    if(result=="timeout"){ 
-        console.log("Sorry, You timed out");  //check if time exceeded the time you passed as argument and print timeout message
-    }else{
-        console.log("Result: %o", result); //print rfid tag UID
-    }
-});
+    rfid.readintime(5000, function(err,result){
+        if(err) console.error("Sorry, some hardware error occurred: %o", err); //some kind of hardware/wire error
+        
+        if(result=="timeout"){ 
+            console.log("Sorry, You timed out");  //check if time exceeded the time you passed as argument and print timeout message
+        }else{
+            console.log("Result: %o", result); //print rfid tag UID
+        }
+    });
+}
