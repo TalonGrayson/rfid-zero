3be1105e72e30722f9b0ab0ec4139c500573c114
Talon Grayson <talongrayson@gmail.com>
2024-08-31 16:07:37 +0100

    Add setTimeout

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 var rfid=require('node-rfid');
 
 for(i=0; i<10; i++) {
-    console.log("Listening to RFID for 5 seconds...");
-    rfid.readintime(5000, function(err,result){
-        if(err) console.error("Sorry, some hardware error occurred: %o", err); //some kind of hardware/wire error
-        
-        if(result=="timeout"){ 
-            console.log("Sorry, You timed out");  //check if time exceeded the time you passed as argument and print timeout message
-        }else{
-            console.log("Result: %o", result); //print rfid tag UID
-        }
-    });
+    setTimeout(5000, function() {
+        console.log("Listening to RFID for 5 seconds...");
+        rfid.readintime(5000, function(err,result){
+            if(err) console.error("Sorry, some hardware error occurred: %o", err); //some kind of hardware/wire error
+            
+            if(result=="timeout"){ 
+                console.log("Sorry, You timed out");  //check if time exceeded the time you passed as argument and print timeout message
+            }else{
+                console.log("Result: %o", result); //print rfid tag UID
+            }
+        });
+    })
 }
